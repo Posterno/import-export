3658492cd1f9b1d2e7c807c365f8d1381b6a71de
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-06-06 19:05:21 +0200

    move assets loader

--- component-loader.php
+++ component-loader.php
@@ -16,6 +16,39 @@ if ( is_admin() ) {
 }
 
 /**
+ * Load assets when appropriate.
+ */
+add_action(
+	'admin_enqueue_scripts',
+	function() {
+
+		$screen = get_current_screen();
+
+		$ids = [
+			'listings_page_schemas_exporter',
+		];
+
+		wp_register_style( 'pno-admin-export-import', PNO_PLUGIN_URL . '/vendor/posterno/import-export/dist/css/screen.css', false, PNO_VERSION );
+		wp_register_script( 'pno-schemas-export', PNO_PLUGIN_URL . '/vendor/posterno/import-export/dist/js/pno-schemas-export.js', array( 'jquery' ), PNO_VERSION, true );
+
+		if ( in_array( $screen->id, $ids ) ) {
+			wp_enqueue_style( 'pno-admin-export-import' );
+			wp_enqueue_script( 'pno-schemas-export' );
+		}
+
+		wp_localize_script(
+			'pno-schemas-export',
+			'pno_schemas_export_params',
+			array(
+				'export_nonce' => wp_create_nonce( 'pno-schemas-export' ),
+			)
+		);
+
+	},
+	20
+);
+
+/**
  * "Register" the export tools.
  */
 add_action(
--- includes/classes/export/Admin.php
+++ includes/classes/export/Admin.php
@@ -75,29 +75,9 @@ class Admin {
 
 		add_action( 'admin_menu', array( $this, 'add_to_menus' ) );
 		//add_action( 'admin_head', array( $this, 'hide_from_menus' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
 		//add_action( 'admin_init', array( $this, 'download_export_file' ) );
 		//add_action( 'wp_ajax_woocommerce_do_ajax_product_export', array( $this, 'do_ajax_product_export' ) );
 
-	}
-
-	/**
-	 * Load assets.
-	 *
-	 * @return void
-	 */
-	public function admin_scripts() {
-
-		wp_register_style( 'pno-export', PNO_PLUGIN_URL . '/vendor/posterno/import-export/dist/css/screen.css', false, PNO_VERSION );
-		wp_register_script( 'pno-schemas-export', PNO_PLUGIN_URL . '/vendor/posterno/import-export/dist/js/pno-schemas-export.js', array( 'jquery' ), PNO_VERSION );
-
-		wp_localize_script(
-			'pno-schemas-export',
-			'pno_schemas_export_params',
-			array(
-				'export_nonce' => wp_create_nonce( 'pno-schemas-export' ),
-			)
-		);
 	}
 
 	/**
