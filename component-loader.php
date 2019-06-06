1dc1e88282cf15d99e3ad49a5160e93859573450
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-06-06 15:41:03 +0200

    update comp

---
+++ component-loader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Hooks the component to WordPress.
+ *
+ * @package     posterno-import-export
+ * @copyright   Copyright (c) 2019, Sematico, LTD
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
