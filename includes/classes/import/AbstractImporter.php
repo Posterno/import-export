f2193056fb8745d9b3d4d630b65d683bc3ffbaac
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-06-12 13:03:06 +0200

    main importer class

---
+++ includes/classes/import/AbstractImporter.php
@@ -0,0 +1,362 @@
+<?php
+/**
+ * Abstract schema importer class.
+ *
+ * @package     posterno-import-export
+ * @copyright   Copyright (c) 2019, Sematico, LTD
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+namespace PosternoImportExport\Import;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Main importer class.
+ */
+abstract class AbstractImporter implements ImporterInterface {
+
+	/**
+	 * CSV file.
+	 *
+	 * @var string
+	 */
+	protected $file = '';
+
+	/**
+	 * The file position after the last read.
+	 *
+	 * @var int
+	 */
+	protected $file_position = 0;
+
+	/**
+	 * Importer parameters.
+	 *
+	 * @var array
+	 */
+	protected $params = array();
+
+	/**
+	 * Raw keys - CSV raw headers.
+	 *
+	 * @var array
+	 */
+	protected $raw_keys = array();
+
+	/**
+	 * Mapped keys - CSV headers.
+	 *
+	 * @var array
+	 */
+	protected $mapped_keys = array();
+
+	/**
+	 * Raw data.
+	 *
+	 * @var array
+	 */
+	protected $raw_data = array();
+
+	/**
+	 * Raw data.
+	 *
+	 * @var array
+	 */
+	protected $file_positions = array();
+
+	/**
+	 * Parsed data.
+	 *
+	 * @var array
+	 */
+	protected $parsed_data = array();
+
+	/**
+	 * Start time of current import.
+	 *
+	 * (default value: 0)
+	 *
+	 * @var int
+	 */
+	protected $start_time = 0;
+
+	/**
+	 * Get file raw headers.
+	 *
+	 * @return array
+	 */
+	public function get_raw_keys() {
+		return $this->raw_keys;
+	}
+
+	/**
+	 * Get file mapped headers.
+	 *
+	 * @return array
+	 */
+	public function get_mapped_keys() {
+		return ! empty( $this->mapped_keys ) ? $this->mapped_keys : $this->raw_keys;
+	}
+
+	/**
+	 * Get raw data.
+	 *
+	 * @return array
+	 */
+	public function get_raw_data() {
+		return $this->raw_data;
+	}
+
+	/**
+	 * Get parsed data.
+	 *
+	 * @return array
+	 */
+	public function get_parsed_data() {
+		return apply_filters( 'posterno_product_importer_parsed_data', $this->parsed_data, $this->get_raw_data() );
+	}
+
+	/**
+	 * Get importer parameters.
+	 *
+	 * @return array
+	 */
+	public function get_params() {
+		return $this->params;
+	}
+
+	/**
+	 * Get file pointer position from the last read.
+	 *
+	 * @return int
+	 */
+	public function get_file_position() {
+		return $this->file_position;
+	}
+
+	/**
+	 * Get file pointer position as a percentage of file size.
+	 *
+	 * @return int
+	 */
+	public function get_percent_complete() {
+		$size = filesize( $this->file );
+		if ( ! $size ) {
+			return 0;
+		}
+
+		return absint( min( round( ( $this->file_position / $size ) * 100 ), 100 ) );
+	}
+
+	/**
+	 * Process a single item and save.
+	 *
+	 * @throws Exception If item cannot be processed.
+	 * @param  array $data Raw CSV data.
+	 * @return array|WP_Error
+	 */
+	protected function process_item( $data ) {
+		try {
+
+			return new WP_Error( 'posterno_product_importer_error', 'Error message process', array( 'status' => 422 ) );
+		} catch ( Exception $e ) {
+			return new WP_Error( 'posterno_product_importer_error', $e->getMessage(), array( 'status' => $e->getCode() ) );
+		}
+	}
+
+	/**
+	 * Get attachment ID.
+	 *
+	 * @param  string $url        Attachment URL.
+	 * @param  int    $product_id Product ID.
+	 * @return int
+	 * @throws Exception If attachment cannot be loaded.
+	 */
+	public function get_attachment_id_from_url( $url, $product_id ) {
+		if ( empty( $url ) ) {
+			return 0;
+		}
+
+		$id         = 0;
+		$upload_dir = wp_upload_dir( null, false );
+		$base_url   = $upload_dir['baseurl'] . '/';
+
+		// Check first if attachment is inside the WordPress uploads directory, or we're given a filename only.
+		if ( false !== strpos( $url, $base_url ) || false === strpos( $url, '://' ) ) {
+			// Search for yyyy/mm/slug.extension or slug.extension - remove the base URL.
+			$file = str_replace( $base_url, '', $url );
+			$args = array(
+				'post_type'   => 'attachment',
+				'post_status' => 'any',
+				'fields'      => 'ids',
+				'meta_query'  => array( // @codingStandardsIgnoreLine.
+					'relation' => 'OR',
+					array(
+						'key'     => '_wp_attached_file',
+						'value'   => '^' . $file,
+						'compare' => 'REGEXP',
+					),
+					array(
+						'key'     => '_wp_attached_file',
+						'value'   => '/' . $file,
+						'compare' => 'LIKE',
+					),
+					array(
+						'key'     => '_pno_attachment_source',
+						'value'   => '/' . $file,
+						'compare' => 'LIKE',
+					),
+				),
+			);
+		} else {
+			// This is an external URL, so compare to source.
+			$args = array(
+				'post_type'   => 'attachment',
+				'post_status' => 'any',
+				'fields'      => 'ids',
+				'meta_query'  => array( // @codingStandardsIgnoreLine.
+					array(
+						'value' => $url,
+						'key'   => '_pno_attachment_source',
+					),
+				),
+			);
+		}
+
+		$ids = get_posts( $args ); // @codingStandardsIgnoreLine.
+
+		if ( $ids ) {
+			$id = current( $ids );
+		}
+
+		// Upload if attachment does not exists.
+		if ( ! $id && stristr( $url, '://' ) ) {
+			$upload = pno_rest_upload_image_from_url( $url );
+
+			if ( is_wp_error( $upload ) ) {
+				throw new Exception( $upload->get_error_message(), 400 );
+			}
+
+			$id = pno_rest_set_uploaded_image_as_attachment( $upload, $product_id );
+
+			if ( ! wp_attachment_is_image( $id ) ) {
+				/* translators: %s: image URL */
+				throw new Exception( sprintf( __( 'Not able to attach "%s".', 'posterno' ), $url ), 400 );
+			}
+
+			// Save attachment source for future reference.
+			update_post_meta( $id, '_pno_attachment_source', $url );
+		}
+
+		if ( ! $id ) {
+			/* translators: %s: image URL */
+			throw new Exception( sprintf( __( 'Unable to use image "%s".', 'posterno' ), $url ), 400 );
+		}
+
+		return $id;
+	}
+
+	/**
+	 * Memory exceeded
+	 *
+	 * Ensures the batch process never exceeds 90%
+	 * of the maximum WordPress memory.
+	 *
+	 * @return bool
+	 */
+	protected function memory_exceeded() {
+		$memory_limit   = $this->get_memory_limit() * 0.9; // 90% of max memory
+		$current_memory = memory_get_usage( true );
+		$return         = false;
+		if ( $current_memory >= $memory_limit ) {
+			$return = true;
+		}
+		return apply_filters( 'posterno_product_importer_memory_exceeded', $return );
+	}
+
+	/**
+	 * Get memory limit
+	 *
+	 * @return int
+	 */
+	protected function get_memory_limit() {
+		if ( function_exists( 'ini_get' ) ) {
+			$memory_limit = ini_get( 'memory_limit' );
+		} else {
+			// Sensible default.
+			$memory_limit = '128M';
+		}
+
+		if ( ! $memory_limit || -1 === intval( $memory_limit ) ) {
+			// Unlimited, set to 32GB.
+			$memory_limit = '32000M';
+		}
+		return intval( $memory_limit ) * 1024 * 1024;
+	}
+
+	/**
+	 * Time exceeded.
+	 *
+	 * Ensures the batch never exceeds a sensible time limit.
+	 * A timeout limit of 30s is common on shared hosting.
+	 *
+	 * @return bool
+	 */
+	protected function time_exceeded() {
+		$finish = $this->start_time + apply_filters( 'posterno_product_importer_default_time_limit', 20 ); // 20 seconds
+		$return = false;
+		if ( time() >= $finish ) {
+			$return = true;
+		}
+		return apply_filters( 'posterno_product_importer_time_exceeded', $return );
+	}
+
+	/**
+	 * Explode CSV cell values using commas by default, and handling escaped
+	 * separators.
+	 *
+	 * @since  3.2.0
+	 * @param  string $value Value to explode.
+	 * @return array
+	 */
+	protected function explode_values( $value ) {
+		$value  = str_replace( '\\,', '::separator::', $value );
+		$values = explode( ',', $value );
+		$values = array_map( array( $this, 'explode_values_formatter' ), $values );
+
+		return $values;
+	}
+
+	/**
+	 * Remove formatting and trim each value.
+	 *
+	 * @since  3.2.0
+	 * @param  string $value Value to format.
+	 * @return string
+	 */
+	protected function explode_values_formatter( $value ) {
+		return trim( str_replace( '::separator::', ',', $value ) );
+	}
+
+	/**
+	 * The exporter prepends a ' to escape fields that start with =, +, - or @.
+	 * Remove the prepended ' character preceding those characters.
+	 *
+	 * @since 3.5.2
+	 * @param  string $value A string that may or may not have been escaped with '.
+	 * @return string
+	 */
+	protected function unescape_data( $value ) {
+		$active_content_triggers = array( "'=", "'+", "'-", "'@" );
+
+		if ( in_array( mb_substr( $value, 0, 2 ), $active_content_triggers, true ) ) {
+			$value = mb_substr( $value, 1 );
+		}
+
+		return $value;
+	}
+
+}
