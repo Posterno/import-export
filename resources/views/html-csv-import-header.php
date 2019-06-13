980bbda6752e9795f904a9ee83873a27bf94cb16
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-06-13 11:29:49 +0100

    add header

--- resources/views/html-csv-import-header.php
+++ resources/views/html-csv-import-header.php
@@ -9,7 +9,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
+<div class="pno-admin-title-area">
+	<div class="wrap">
+		<h2><?php esc_html_e( 'Import data', 'posterno' ); ?></h2>
+		<ul class="title-links hidden-sm-and-down">
+			<li>
+				<a href="https://posterno.com/addons" rel="nofollow" target="_blank" class="page-title-action"><?php esc_html_e( 'View Addons', 'posterno' ); ?></a>
+			</li>
+			<li>
+				<a href="https://docs.posterno.com/" rel="nofollow" target="_blank" class="page-title-action"><?php esc_html_e( 'Documentation', 'posterno' ); ?></a>
+			</li>
+		</ul>
+	</div>
+</div>
+
 <div class="wrap posterno">
-	<h1><?php esc_html_e( 'Import Products', 'posterno' ); ?></h1>
+	<h1 class="screen-reader-text"><?php esc_html_e( 'Import data', 'posterno' ); ?></h1>
 
 	<div class="posterno-progress-form-wrapper">
